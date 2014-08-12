1caeedec45c49c62b5b532bd1afb3da98ab8fc8d
1248416@qq.com <by.rain1125@gmail.com>
2014-08-12 18:37:46 +0800

    caculate the max sum of subsequence. Done.

--- [DP]Max_sum_subsequence.cpp
+++ [DP]Max_sum_subsequence.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 using namespace std;
 
+int get_max_sum(int* a, int n){
+	int sum = a[0];
+	int sum_temp = a[0];
+	for(int i = 0;i < n;i++){
+		if(a[i] < (sum_temp + a[i]))
+			sum_temp = sum_temp + a[i];
+		else
+			sum_temp = a[i];
+		if (sum < sum_temp)
+			sum = sum_temp;
+	}
+	return sum;
+}
+
 int main(int argc, char* argv[]){
-	cout << "123";
+	int a[] =  {-2, 11, -4, 13, -5, -2, 100, -99, 100};
+	int n = sizeof(a)/sizeof(int);
+	int sum = get_max_sum(a, n);
+	cout << sum;
 	return 0;
 }
